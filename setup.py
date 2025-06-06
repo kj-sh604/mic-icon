57b244a0cc016f59547b381ccbea483e9b0fa6a1
kj-sh604 <43.splash@gmail.com>
2025-06-06 17:53:24 -0400

    refactor: anchor off of the `requirements.txt`

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='mic-icon',
     version='1.0',
     packages=['mic_icon'],
-    install_requires=[
-        'pulsectl',
-        'pygobject'
-    ],
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'mic-icon=mic_icon.__main__:main',
